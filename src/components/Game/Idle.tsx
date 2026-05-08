5a178433ee2f71cdd1dfa45e62fc7f4c241a4f8b
Benita Almroth <benita.almroth@hotmail.com>
2026-05-08 12:09:52 +0200

    minimal base for mobile css

--- src/components/Game/Idle.tsx
+++ src/components/Game/Idle.tsx
@@ -4,8 +4,8 @@ import type { IdleProps } from "../../types/Game";
 export function Idle({ onStartGame }: IdleProps): ReactNode {
   return (
     <div>
-      <h1>FIND THE IMPOSTOR</h1>
-      <h1>Wanted!</h1>
+      <h1>WANTED</h1>
+      <h2>AT YRGO!</h2>
       <button onClick={onStartGame}>Start game</button>
     </div>
   );
