9ad05838c41998394fddcf38e3cb3670b3ed6ea1
Benita Almroth <benita.almroth@hotmail.com>
2026-05-11 10:10:46 +0200

    added background, started css on starting page

--- src/components/Game/Idle.tsx
+++ src/components/Game/Idle.tsx
@@ -1,11 +1,14 @@
 import type { ReactNode } from "react";
 import type { IdleProps } from "../../types/Game";
+import styles from "./Idle.module.css";
 
 export function Idle({ onStartGame }: IdleProps): ReactNode {
   return (
     <div>
       <h1>WANTED</h1>
-      <h2>AT YRGO!</h2>
+      <h2>
+        AT <span className={styles.yrgo}>YRGO</span>!
+      </h2>
       <button onClick={onStartGame}>Start game</button>
     </div>
   );
