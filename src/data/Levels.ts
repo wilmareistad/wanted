3e66a53d0dd35b40222f09d6292c268e0d5deaf4
Benita Almroth <benita.almroth@hotmail.com>
2026-05-11 11:38:49 +0200

    reworked carousel to move seamlessly

--- src/data/Levels.ts
+++ src/data/Levels.ts
@@ -1,17 +1,17 @@
 import type { LevelConfig } from "../types/Level";
 
-// export const LEVELS: LevelConfig[] = [
-//   { level: 1, gridCount: 4 },
-//   { level: 2, gridCount: 9 },
-//   { level: 3, gridCount: 16 },
-//   { level: 4, gridCount: 25 },
-//   { level: 5, gridCount: 16, carousel: true },
-//   { level: 6, gridCount: 25, carousel: true },
-//   { level: 7, gridCount: 25, carousel: true },
-// ];
-
 export const LEVELS: LevelConfig[] = [
+  { level: 1, gridCount: 4 },
+  { level: 2, gridCount: 9 },
+  { level: 3, gridCount: 16 },
+  { level: 4, gridCount: 25 },
   { level: 5, gridCount: 16, carousel: true },
   { level: 6, gridCount: 25, carousel: true },
   { level: 7, gridCount: 25, carousel: true },
 ];
+
+// export const LEVELS: LevelConfig[] = [
+//   { level: 5, gridCount: 16, carousel: true },
+//   { level: 6, gridCount: 25, carousel: true },
+//   { level: 7, gridCount: 25, carousel: true },
+// ];
