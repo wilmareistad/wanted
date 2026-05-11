0ed45c6a1f26f862bed084e91d9e30b2fcda8150
Benita Almroth <benita.almroth@hotmail.com>
2026-05-11 10:16:10 +0200

    added base html for idle

--- src/components/Game/Idle.tsx
+++ src/components/Game/Idle.tsx
@@ -9,6 +9,14 @@ export function Idle({ onStartGame }: IdleProps): ReactNode {
       <h2>
         AT <span className={styles.yrgo}>YRGO</span>!
       </h2>
+
+      <section className="styles.InfoBoxes">
+        <div className="styles.instructions">instruktioner ruta</div>
+        <div className="styles.leaderboard">leaderbord ruta</div>
+      </section>
+
+      <section className="styles.idleButtons">sektion med alla knappar</section>
+
       <button onClick={onStartGame}>Start game</button>
     </div>
   );
