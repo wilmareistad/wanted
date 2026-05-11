aeb4be8c32de28a904bdca68dee2197f24faa731
Wilma <wilmareistad@icloud.com>
2026-05-11 10:57:23 +0200

    fixed style for info boxes

--- src/components/Game/Idle.tsx
+++ src/components/Game/Idle.tsx
@@ -4,18 +4,24 @@ import styles from "./Idle.module.css";
 
 export function Idle({ onStartGame }: IdleProps): ReactNode {
   return (
-    <div>
+    <div className={styles.idleContainer}>
       <h1>WANTED</h1>
       <h2>
         AT <span className={styles.yrgo}>YRGO</span>!
       </h2>
 
-      <section className="styles.InfoBoxes">
-        <div className="styles.instructions">instruktioner ruta</div>
-        <div className="styles.leaderboard">leaderbord ruta</div>
+      <section className={styles.infoBoxes}>
+        <div className={styles.box}>
+          <h3>INSTRUCTIONS</h3>
+          <p>Runes family is visiting Yrgo but one is an imposter... Find the wanted Rune and catch them before time runs out! </p>
+        </div>
+        <div className={styles.box}>
+          <h3 className={styles.leaderboardH}>LEADERBOARD</h3>
+          <p>leaderbord ruta</p>
+        </div>
       </section>
 
-      <section className="styles.idleButtons">sektion med alla knappar</section>
+      <section className={styles.boxButtons}>sektion med alla knappar</section>
 
       <button onClick={onStartGame}>Start game</button>
     </div>
