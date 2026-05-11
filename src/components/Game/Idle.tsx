020701cf47e20ff73a102e5d7d3be5d8e20582a8
Wilma <wilmareistad@icloud.com>
2026-05-11 11:22:10 +0200

    added style for buttons

--- src/components/Game/Idle.tsx
+++ src/components/Game/Idle.tsx
@@ -13,7 +13,10 @@ export function Idle({ onStartGame }: IdleProps): ReactNode {
       <section className={styles.infoBoxes}>
         <div className={styles.box}>
           <h3>INSTRUCTIONS</h3>
-          <p>Runes family is visiting Yrgo but one is an imposter... Find the wanted Rune and catch them before time runs out! </p>
+          <p>
+            Runes family is visiting Yrgo but one is an imposter... Find the
+            wanted Rune and catch them before time runs out!{" "}
+          </p>
         </div>
         <div className={styles.box}>
           <h3 className={styles.leaderboardH}>LEADERBOARD</h3>
@@ -21,9 +24,13 @@ export function Idle({ onStartGame }: IdleProps): ReactNode {
         </div>
       </section>
 
-      <section className={styles.boxButtons}>sektion med alla knappar</section>
-
-      <button onClick={onStartGame}>Start game</button>
+      <section className={styles.boxButtons}>
+        <button className={styles.playBtn} onClick={onStartGame}>PLAY</button>
+        <div className={styles.infoButtons}>
+          <button className={styles.tivoliBtn}>TIVOLI</button>
+          <button className={styles.rewardBtn}>REWARDS?</button>
+        </div>
+      </section>
     </div>
   );
 }
