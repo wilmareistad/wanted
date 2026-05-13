0c464b9a3e8aea96789b2fba2243e6fa12a704fb
Benita Almroth <benita.almroth@hotmail.com>
2026-05-13 08:58:01 +0200

    simple ui for gameover screen

--- src/components/Game/GameOver.tsx
+++ src/components/Game/GameOver.tsx
@@ -1,12 +1,49 @@
 import type { ReactNode } from "react";
 import type { GameOverProps } from "../../types/Game";
+import styles from "./GameOver.module.css";
+import { Leaderboard } from "../Leaderboard";
+import { Navigation } from "./Navigation";
 
-export function GameOver({ score, onPlayAgain }: GameOverProps): ReactNode {
+export function GameOver({
+  score,
+  currentLevel,
+  onPlayAgain,
+}: GameOverProps): ReactNode {
   return (
-    <div>
-      <h2>Game Over!</h2>
-      <p>Score: {score}</p>
-      <button onClick={onPlayAgain}>Play again</button>
+    <div className={styles.overContainer}>
+      <h1>GAME OVER</h1>
+
+<section className={styles.infoBoxes}>
+
+<div className={styles.box}>
+  <div className={styles.statsRow}>
+    <div className={styles.statItem}>
+      <span className={styles.statLabel}>Score:</span>
+      <span className={styles.statValue}>{score}</span>
+    </div>
+
+    <div className={styles.statItem}>
+      <span className={styles.statLabel}>Level:</span>
+      <span className={styles.statValue}>{currentLevel.level}</span>
+    </div>
+  </div>
+
+  <div className={styles.rewardBox}>
+    <h3 className={styles.rewardTitle}>Reward</h3>
+    <div>euro: stamp:
+    </div>
+  </div>
+</div>
+
+
+        <div className={styles.box}>
+          <h3 className={styles.leaderboardH}>LEADERBOARD</h3>
+          <Leaderboard />
+        </div>
+      </section>
+
+
+      <Navigation onStartGame={onPlayAgain} />
     </div>
   );
 }
